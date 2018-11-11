057884767a34145d4f3e0176e9f9e57886eb459b
Arnaud Cornet <arnaud.cornet@gmail.com>
2018-11-11 11:39:24 -0500

    Fix typo.

--- box-wireguard.sh
+++ box-wireguard.sh
@@ -6,7 +6,7 @@ set -eu
 cd $(dirname $0)
 source ./box-lib.sh
 
-set-no-pdiff
+no-pdiff
 echo "deb http://deb.debian.org/debian/ unstable main" \
 	> /etc/apt/sources.list.d/unstable-wireguard.list
 printf 'Package: *\nPin: release a=unstable\nPin-Priority: 150\n' \
